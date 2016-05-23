6bc762aa097ae325271027879bafe3e5deb61c3c
wetrolam <wetrolam@gmail.com>
2016-05-23 15:23:49 +0200

    rename url for creating an email

--- emails/urls.py
+++ emails/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 #app_name = 'emails'
 urlpatterns = [
-    url(r'^new/$', views.EmailCreateView.as_view(), name='new'),
+    url(r'^create/$', views.EmailCreateView.as_view(), name='new'),
     url(r'^sent$', views.sent),
 	url(r'^$', views.ListView.as_view(), name='list'),
 	url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
