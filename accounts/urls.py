a101a865716449883db027c388c922ede86cec26
wetrolam <wetrolam@gmail.com>
2016-06-05 14:03:10 +0200

    added password changed view

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
 	#url(r'^logout/$', auth_views.logout_then_login), #, {'template_name': 'accounts/logout.html'}),
 	url(r'^create/$', views.create_account),
 	url(r'^profile/$', views.AccountProfileView.as_view()),
+	url(r'^password_change/$', auth_views.password_change, {'template_name': 'accounts/password_change.html', 'post_change_redirect': '/accounts/profile'}), #, name = 'password_change'),
 ]
