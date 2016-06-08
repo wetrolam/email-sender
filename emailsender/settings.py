b826ad933e648a25cafef3589fb3dc6538d4ea47
wetrolam <wetrolam@gmail.com>
2016-06-08 21:38:36 +0200

    application accounts added to INSTALLED_APPS (fixed error)

--- emailsender/settings.py
+++ emailsender/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts.apps.AccountsConfig',
     'emails.apps.EmailsConfig',
     'anymail',
     'django.contrib.admin',
